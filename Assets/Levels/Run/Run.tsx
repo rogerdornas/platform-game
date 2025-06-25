17cf64f55e35bb58a8cdea881367cd39b7cd6abf
Vinícius Alexandre <vinicius.alex89@gmail.com>
2025-06-25 18:06:36 -0300

    Add Run assets
    Sprites da fase Run, 1ª versão da fase

---
+++ Assets/Levels/Run/Run.tsx
@@ -0,0 +1,150 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Run" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <tile id="0">
+  <image source="../../Sprites/Run/01.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="../../Sprites/Run/02.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="../../Sprites/Run/03.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="../../Sprites/Run/04.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="../../Sprites/Run/05.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="../../Sprites/Run/06.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="../../Sprites/Run/07.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="../../Sprites/Run/08.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="../../Sprites/Run/09.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="../../Sprites/Run/10.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="../../Sprites/Run/11.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="../../Sprites/Run/12.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="../../Sprites/Run/13.png" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="../../Sprites/Run/14.png" width="32" height="32"/>
+ </tile>
+ <tile id="14">
+  <image source="../../Sprites/Run/15.png" width="32" height="32"/>
+ </tile>
+ <tile id="15">
+  <image source="../../Sprites/Run/16.png" width="32" height="32"/>
+ </tile>
+ <tile id="16">
+  <image source="../../Sprites/Run/17.png" width="32" height="32"/>
+ </tile>
+ <tile id="17">
+  <image source="../../Sprites/Run/18.png" width="32" height="32"/>
+ </tile>
+ <tile id="18">
+  <image source="../../Sprites/Run/19.png" width="32" height="32"/>
+ </tile>
+ <tile id="19">
+  <image source="../../Sprites/Run/20.png" width="32" height="32"/>
+ </tile>
+ <tile id="20">
+  <image source="../../Sprites/Run/21.png" width="32" height="32"/>
+ </tile>
+ <tile id="21">
+  <image source="../../Sprites/Run/22.png" width="32" height="32"/>
+ </tile>
+ <tile id="22">
+  <image source="../../Sprites/Run/23.png" width="32" height="32"/>
+ </tile>
+ <tile id="23">
+  <image source="../../Sprites/Run/24.png" width="32" height="32"/>
+ </tile>
+ <tile id="24">
+  <image source="../../Sprites/Run/25.png" width="32" height="32"/>
+ </tile>
+ <tile id="25">
+  <image source="../../Sprites/Run/26.png" width="32" height="32"/>
+ </tile>
+ <tile id="26">
+  <image source="../../Sprites/Run/27.png" width="32" height="32"/>
+ </tile>
+ <tile id="27">
+  <image source="../../Sprites/Run/28.png" width="32" height="32"/>
+ </tile>
+ <tile id="28">
+  <image source="../../Sprites/Run/29.png" width="32" height="32"/>
+ </tile>
+ <tile id="29">
+  <image source="../../Sprites/Run/30.png" width="32" height="32"/>
+ </tile>
+ <tile id="30">
+  <image source="../../Sprites/Run/31.png" width="32" height="32"/>
+ </tile>
+ <tile id="31">
+  <image source="../../Sprites/Run/32.png" width="32" height="32"/>
+ </tile>
+ <tile id="32">
+  <image source="../../Sprites/Run/33.png" width="32" height="32"/>
+ </tile>
+ <tile id="33">
+  <image source="../../Sprites/Run/34.png" width="32" height="32"/>
+ </tile>
+ <tile id="34">
+  <image source="../../Sprites/Run/35.png" width="32" height="32"/>
+ </tile>
+ <tile id="35">
+  <image source="../../Sprites/Run/36.png" width="32" height="32"/>
+ </tile>
+ <tile id="36">
+  <image source="../../Sprites/Run/37.png" width="32" height="32"/>
+ </tile>
+ <tile id="37">
+  <image source="../../Sprites/Run/38.png" width="32" height="32"/>
+ </tile>
+ <tile id="38">
+  <image source="../../Sprites/Run/39.png" width="32" height="32"/>
+ </tile>
+ <tile id="39">
+  <image source="../../Sprites/Run/40.png" width="32" height="32"/>
+ </tile>
+ <tile id="40">
+  <image source="../../Sprites/Run/41.png" width="32" height="32"/>
+ </tile>
+ <tile id="41">
+  <image source="../../Sprites/Run/42.png" width="32" height="32"/>
+ </tile>
+ <tile id="42">
+  <image source="../../Sprites/Run/43.png" width="32" height="32"/>
+ </tile>
+ <tile id="43">
+  <image source="../../Sprites/Run/44.png" width="32" height="32"/>
+ </tile>
+ <tile id="44">
+  <image source="../../Sprites/Run/45.png" width="32" height="32"/>
+ </tile>
+ <tile id="45">
+  <image source="../../Sprites/Run/46.png" width="32" height="32"/>
+ </tile>
+ <tile id="46">
+  <image source="../../Sprites/Run/47.png" width="32" height="32"/>
+ </tile>
+ <tile id="47">
+  <image source="../../Sprites/Run/48.png" width="32" height="32"/>
+ </tile>
+ <tile id="48">
+  <image source="../../Sprites/Run/49.png" width="32" height="32"/>
+ </tile>
+</tileset>
