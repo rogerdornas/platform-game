8b21a90c07850a381b0c4be0854652895805d881
Vinícius Alexandre <vinicius.alex89@gmail.com>
2025-06-29 11:22:53 -0300

    Fase pain sprites

--- Assets/Levels/Pain/Pain.tsx
+++ Assets/Levels/Pain/Pain.tsx
@@ -1,4 +1,240 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Pain" tilewidth="32" tileheight="32" tilecount="54" columns="6">
- <image source="Pain.png" width="192" height="288"/>
+<tileset version="1.10" tiledversion="1.11.2" name="Pain" tilewidth="32" tileheight="32" tilecount="79" columns="10">
+ <tile id="0">
+  <image source="../../Sprites/Pain/01.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="../../Sprites/Pain/02.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="../../Sprites/Pain/03.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="../../Sprites/Pain/04.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="../../Sprites/Pain/05.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="../../Sprites/Pain/06.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="../../Sprites/Pain/07.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="../../Sprites/Pain/08.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="../../Sprites/Pain/09.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="../../Sprites/Pain/10.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="../../Sprites/Pain/11.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="../../Sprites/Pain/12.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="../../Sprites/Pain/13.png" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="../../Sprites/Pain/14.png" width="32" height="32"/>
+ </tile>
+ <tile id="14">
+  <image source="../../Sprites/Pain/15.png" width="32" height="32"/>
+ </tile>
+ <tile id="15">
+  <image source="../../Sprites/Pain/16.png" width="32" height="32"/>
+ </tile>
+ <tile id="16">
+  <image source="../../Sprites/Pain/17.png" width="32" height="32"/>
+ </tile>
+ <tile id="17">
+  <image source="../../Sprites/Pain/18.png" width="32" height="32"/>
+ </tile>
+ <tile id="18">
+  <image source="../../Sprites/Pain/19.png" width="32" height="32"/>
+ </tile>
+ <tile id="19">
+  <image source="../../Sprites/Pain/20.png" width="32" height="32"/>
+ </tile>
+ <tile id="20">
+  <image source="../../Sprites/Pain/21.png" width="32" height="32"/>
+ </tile>
+ <tile id="21">
+  <image source="../../Sprites/Pain/22.png" width="32" height="32"/>
+ </tile>
+ <tile id="22">
+  <image source="../../Sprites/Pain/23.png" width="32" height="32"/>
+ </tile>
+ <tile id="23">
+  <image source="../../Sprites/Pain/24.png" width="32" height="32"/>
+ </tile>
+ <tile id="24">
+  <image source="../../Sprites/Pain/25.png" width="32" height="32"/>
+ </tile>
+ <tile id="25">
+  <image source="../../Sprites/Pain/26.png" width="32" height="32"/>
+ </tile>
+ <tile id="26">
+  <image source="../../Sprites/Pain/27.png" width="32" height="32"/>
+ </tile>
+ <tile id="27">
+  <image source="../../Sprites/Pain/28.png" width="32" height="32"/>
+ </tile>
+ <tile id="28">
+  <image source="../../Sprites/Pain/29.png" width="32" height="32"/>
+ </tile>
+ <tile id="29">
+  <image source="../../Sprites/Pain/30.png" width="32" height="32"/>
+ </tile>
+ <tile id="30">
+  <image source="../../Sprites/Pain/31.png" width="32" height="32"/>
+ </tile>
+ <tile id="31">
+  <image source="../../Sprites/Pain/32.png" width="32" height="32"/>
+ </tile>
+ <tile id="32">
+  <image source="../../Sprites/Pain/33.png" width="32" height="32"/>
+ </tile>
+ <tile id="33">
+  <image source="../../Sprites/Pain/34.png" width="32" height="32"/>
+ </tile>
+ <tile id="34">
+  <image source="../../Sprites/Pain/35.png" width="32" height="32"/>
+ </tile>
+ <tile id="35">
+  <image source="../../Sprites/Pain/36.png" width="32" height="32"/>
+ </tile>
+ <tile id="36">
+  <image source="../../Sprites/Pain/37.png" width="32" height="32"/>
+ </tile>
+ <tile id="37">
+  <image source="../../Sprites/Pain/38.png" width="32" height="32"/>
+ </tile>
+ <tile id="38">
+  <image source="../../Sprites/Pain/39.png" width="32" height="32"/>
+ </tile>
+ <tile id="39">
+  <image source="../../Sprites/Pain/40.png" width="32" height="32"/>
+ </tile>
+ <tile id="40">
+  <image source="../../Sprites/Pain/41.png" width="32" height="32"/>
+ </tile>
+ <tile id="41">
+  <image source="../../Sprites/Pain/42.png" width="32" height="32"/>
+ </tile>
+ <tile id="42">
+  <image source="../../Sprites/Pain/43.png" width="32" height="32"/>
+ </tile>
+ <tile id="43">
+  <image source="../../Sprites/Pain/44.png" width="32" height="32"/>
+ </tile>
+ <tile id="44">
+  <image source="../../Sprites/Pain/45.png" width="32" height="32"/>
+ </tile>
+ <tile id="45">
+  <image source="../../Sprites/Pain/46.png" width="32" height="32"/>
+ </tile>
+ <tile id="46">
+  <image source="../../Sprites/Pain/47.png" width="32" height="32"/>
+ </tile>
+ <tile id="47">
+  <image source="../../Sprites/Pain/48.png" width="32" height="32"/>
+ </tile>
+ <tile id="48">
+  <image source="../../Sprites/Pain/49.png" width="32" height="32"/>
+ </tile>
+ <tile id="49">
+  <image source="../../Sprites/Pain/50.png" width="32" height="32"/>
+ </tile>
+ <tile id="50">
+  <image source="../../Sprites/Pain/51.png" width="32" height="32"/>
+ </tile>
+ <tile id="51">
+  <image source="../../Sprites/Pain/52.png" width="32" height="32"/>
+ </tile>
+ <tile id="52">
+  <image source="../../Sprites/Pain/53.png" width="32" height="32"/>
+ </tile>
+ <tile id="53">
+  <image source="../../Sprites/Pain/54.png" width="32" height="32"/>
+ </tile>
+ <tile id="54">
+  <image source="../../Sprites/Pain/55.png" width="32" height="32"/>
+ </tile>
+ <tile id="55">
+  <image source="../../Sprites/Pain/56.png" width="32" height="32"/>
+ </tile>
+ <tile id="56">
+  <image source="../../Sprites/Pain/57.png" width="32" height="32"/>
+ </tile>
+ <tile id="57">
+  <image source="../../Sprites/Pain/58.png" width="32" height="32"/>
+ </tile>
+ <tile id="58">
+  <image source="../../Sprites/Pain/59.png" width="32" height="32"/>
+ </tile>
+ <tile id="59">
+  <image source="../../Sprites/Pain/60.png" width="32" height="32"/>
+ </tile>
+ <tile id="60">
+  <image source="../../Sprites/Pain/61.png" width="32" height="32"/>
+ </tile>
+ <tile id="61">
+  <image source="../../Sprites/Pain/62.png" width="32" height="32"/>
+ </tile>
+ <tile id="62">
+  <image source="../../Sprites/Pain/63.png" width="32" height="32"/>
+ </tile>
+ <tile id="63">
+  <image source="../../Sprites/Pain/64.png" width="32" height="32"/>
+ </tile>
+ <tile id="64">
+  <image source="../../Sprites/Pain/65.png" width="32" height="32"/>
+ </tile>
+ <tile id="65">
+  <image source="../../Sprites/Pain/66.png" width="32" height="32"/>
+ </tile>
+ <tile id="66">
+  <image source="../../Sprites/Pain/67.png" width="32" height="32"/>
+ </tile>
+ <tile id="67">
+  <image source="../../Sprites/Pain/68.png" width="32" height="32"/>
+ </tile>
+ <tile id="68">
+  <image source="../../Sprites/Pain/69.png" width="32" height="32"/>
+ </tile>
+ <tile id="69">
+  <image source="../../Sprites/Pain/70.png" width="32" height="32"/>
+ </tile>
+ <tile id="70">
+  <image source="../../Sprites/Pain/71.png" width="32" height="32"/>
+ </tile>
+ <tile id="71">
+  <image source="../../Sprites/Pain/72.png" width="32" height="32"/>
+ </tile>
+ <tile id="72">
+  <image source="../../Sprites/Pain/73.png" width="32" height="32"/>
+ </tile>
+ <tile id="73">
+  <image source="../../Sprites/Pain/74.png" width="32" height="32"/>
+ </tile>
+ <tile id="74">
+  <image source="../../Sprites/Pain/75.png" width="32" height="32"/>
+ </tile>
+ <tile id="75">
+  <image source="../../Sprites/Pain/76.png" width="32" height="32"/>
+ </tile>
+ <tile id="76">
+  <image source="../../Sprites/Pain/77.png" width="32" height="32"/>
+ </tile>
+ <tile id="77">
+  <image source="../../Sprites/Pain/78.png" width="32" height="32"/>
+ </tile>
+ <tile id="78">
+  <image source="../../Sprites/Pain/79.png" width="32" height="32"/>
+ </tile>
 </tileset>
