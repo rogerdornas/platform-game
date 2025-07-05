9c2f11ffea215e602d23cc308670d4dd8e50dc1d
Vinícius Alexandre <vinicius.alex89@gmail.com>
2025-07-05 10:43:12 -0300

    Ajuste em swamp

--- Assets/Levels/3-Swamp/Swamp.tsx
+++ Assets/Levels/3-Swamp/Swamp.tsx
@@ -1,244 +1,244 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Swamp" tilewidth="32" tileheight="32" tilecount="116" columns="10">
  <tile id="0">
-  <image source="../../Sprites/Swamp/01.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/001.png" width="32" height="32"/>
  </tile>
  <tile id="1">
-  <image source="../../Sprites/Swamp/02.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/002.png" width="32" height="32"/>
  </tile>
  <tile id="2">
-  <image source="../../Sprites/Swamp/03.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/003.png" width="32" height="32"/>
  </tile>
  <tile id="3">
-  <image source="../../Sprites/Swamp/04.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/004.png" width="32" height="32"/>
  </tile>
  <tile id="4">
-  <image source="../../Sprites/Swamp/05.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/005.png" width="32" height="32"/>
  </tile>
  <tile id="5">
-  <image source="../../Sprites/Swamp/06.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/006.png" width="32" height="32"/>
  </tile>
  <tile id="6">
-  <image source="../../Sprites/Swamp/07.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/007.png" width="32" height="32"/>
  </tile>
  <tile id="7">
-  <image source="../../Sprites/Swamp/08.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/008.png" width="32" height="32"/>
  </tile>
  <tile id="8">
-  <image source="../../Sprites/Swamp/09.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/009.png" width="32" height="32"/>
  </tile>
  <tile id="9">
-  <image source="../../Sprites/Swamp/10.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/010.png" width="32" height="32"/>
  </tile>
  <tile id="10">
-  <image source="../../Sprites/Swamp/11.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/011.png" width="32" height="32"/>
  </tile>
  <tile id="11">
-  <image source="../../Sprites/Swamp/12.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/012.png" width="32" height="32"/>
  </tile>
  <tile id="12">
-  <image source="../../Sprites/Swamp/13.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/013.png" width="32" height="32"/>
  </tile>
  <tile id="13">
-  <image source="../../Sprites/Swamp/14.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/014.png" width="32" height="32"/>
  </tile>
  <tile id="14">
-  <image source="../../Sprites/Swamp/15.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/015.png" width="32" height="32"/>
  </tile>
  <tile id="15">
-  <image source="../../Sprites/Swamp/16.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/016.png" width="32" height="32"/>
  </tile>
  <tile id="16">
-  <image source="../../Sprites/Swamp/17.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/017.png" width="32" height="32"/>
  </tile>
  <tile id="17">
-  <image source="../../Sprites/Swamp/18.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/018.png" width="32" height="32"/>
  </tile>
  <tile id="18">
-  <image source="../../Sprites/Swamp/19.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/019.png" width="32" height="32"/>
  </tile>
  <tile id="19">
-  <image source="../../Sprites/Swamp/20.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/020.png" width="32" height="32"/>
  </tile>
  <tile id="20">
-  <image source="../../Sprites/Swamp/21.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/021.png" width="32" height="32"/>
  </tile>
  <tile id="21">
-  <image source="../../Sprites/Swamp/22.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/022.png" width="32" height="32"/>
  </tile>
  <tile id="22">
-  <image source="../../Sprites/Swamp/23.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/023.png" width="32" height="32"/>
  </tile>
  <tile id="23">
-  <image source="../../Sprites/Swamp/24.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/024.png" width="32" height="32"/>
  </tile>
  <tile id="24">
-  <image source="../../Sprites/Swamp/25.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/025.png" width="32" height="32"/>
  </tile>
  <tile id="25">
-  <image source="../../Sprites/Swamp/26.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/026.png" width="32" height="32"/>
  </tile>
  <tile id="26">
-  <image source="../../Sprites/Swamp/27.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/027.png" width="32" height="32"/>
  </tile>
  <tile id="27">
-  <image source="../../Sprites/Swamp/28.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/028.png" width="32" height="32"/>
  </tile>
  <tile id="28">
-  <image source="../../Sprites/Swamp/29.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/029.png" width="32" height="32"/>
  </tile>
  <tile id="29">
-  <image source="../../Sprites/Swamp/30.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/030.png" width="32" height="32"/>
  </tile>
  <tile id="30">
-  <image source="../../Sprites/Swamp/31.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/031.png" width="32" height="32"/>
  </tile>
  <tile id="31">
-  <image source="../../Sprites/Swamp/32.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/032.png" width="32" height="32"/>
  </tile>
  <tile id="32">
-  <image source="../../Sprites/Swamp/33.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/033.png" width="32" height="32"/>
  </tile>
  <tile id="33">
-  <image source="../../Sprites/Swamp/34.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/034.png" width="32" height="32"/>
  </tile>
  <tile id="34">
-  <image source="../../Sprites/Swamp/35.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/035.png" width="32" height="32"/>
  </tile>
  <tile id="35">
-  <image source="../../Sprites/Swamp/36.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/036.png" width="32" height="32"/>
  </tile>
  <tile id="36">
-  <image source="../../Sprites/Swamp/37.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/037.png" width="32" height="32"/>
  </tile>
  <tile id="37">
-  <image source="../../Sprites/Swamp/38.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/038.png" width="32" height="32"/>
  </tile>
  <tile id="38">
-  <image source="../../Sprites/Swamp/39.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/039.png" width="32" height="32"/>
  </tile>
  <tile id="39">
-  <image source="../../Sprites/Swamp/40.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/040.png" width="32" height="32"/>
  </tile>
  <tile id="40">
-  <image source="../../Sprites/Swamp/41.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/041.png" width="32" height="32"/>
  </tile>
  <tile id="41">
-  <image source="../../Sprites/Swamp/42.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/042.png" width="32" height="32"/>
  </tile>
  <tile id="42">
-  <image source="../../Sprites/Swamp/43.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/043.png" width="32" height="32"/>
  </tile>
  <tile id="43">
-  <image source="../../Sprites/Swamp/44.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/044.png" width="32" height="32"/>
  </tile>
  <tile id="44">
-  <image source="../../Sprites/Swamp/45.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/045.png" width="32" height="32"/>
  </tile>
  <tile id="45">
-  <image source="../../Sprites/Swamp/46.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/046.png" width="32" height="32"/>
  </tile>
  <tile id="46">
-  <image source="../../Sprites/Swamp/47.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/047.png" width="32" height="32"/>
  </tile>
  <tile id="47">
-  <image source="../../Sprites/Swamp/48.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/048.png" width="32" height="32"/>
  </tile>
  <tile id="48">
-  <image source="../../Sprites/Swamp/49.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/049.png" width="32" height="32"/>
  </tile>
  <tile id="49">
-  <image source="../../Sprites/Swamp/50.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/050.png" width="32" height="32"/>
  </tile>
  <tile id="50">
-  <image source="../../Sprites/Swamp/51.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/051.png" width="32" height="32"/>
  </tile>
  <tile id="51">
-  <image source="../../Sprites/Swamp/52.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/052.png" width="32" height="32"/>
  </tile>
  <tile id="52">
-  <image source="../../Sprites/Swamp/53.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/053.png" width="32" height="32"/>
  </tile>
  <tile id="53">
-  <image source="../../Sprites/Swamp/54.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/054.png" width="32" height="32"/>
  </tile>
  <tile id="54">
-  <image source="../../Sprites/Swamp/55.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/055.png" width="32" height="32"/>
  </tile>
  <tile id="55">
-  <image source="../../Sprites/Swamp/56.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/056.png" width="32" height="32"/>
  </tile>
  <tile id="56">
-  <image source="../../Sprites/Swamp/57.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/057.png" width="32" height="32"/>
  </tile>
  <tile id="57">
-  <image source="../../Sprites/Swamp/58.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/058.png" width="32" height="32"/>
  </tile>
  <tile id="58">
-  <image source="../../Sprites/Swamp/59.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/059.png" width="32" height="32"/>
  </tile>
  <tile id="59">
-  <image source="../../Sprites/Swamp/60.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/060.png" width="32" height="32"/>
  </tile>
  <tile id="60">
-  <image source="../../Sprites/Swamp/61.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/061.png" width="32" height="32"/>
  </tile>
  <tile id="61">
-  <image source="../../Sprites/Swamp/62.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/062.png" width="32" height="32"/>
  </tile>
  <tile id="62">
-  <image source="../../Sprites/Swamp/63.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/063.png" width="32" height="32"/>
  </tile>
  <tile id="63">
-  <image source="../../Sprites/Swamp/64.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/064.png" width="32" height="32"/>
  </tile>
  <tile id="64">
-  <image source="../../Sprites/Swamp/65.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/065.png" width="32" height="32"/>
  </tile>
  <tile id="65">
-  <image source="../../Sprites/Swamp/66.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/066.png" width="32" height="32"/>
  </tile>
  <tile id="66">
-  <image source="../../Sprites/Swamp/67.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/067.png" width="32" height="32"/>
  </tile>
  <tile id="67">
-  <image source="../../Sprites/Swamp/68.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/068.png" width="32" height="32"/>
  </tile>
  <tile id="68">
-  <image source="../../Sprites/Swamp/69.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/069.png" width="32" height="32"/>
  </tile>
  <tile id="69">
-  <image source="../../Sprites/Swamp/70.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/070.png" width="32" height="32"/>
  </tile>
  <tile id="70">
-  <image source="../../Sprites/Swamp/71.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/071.png" width="32" height="32"/>
  </tile>
  <tile id="71">
-  <image source="../../Sprites/Swamp/72.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/072.png" width="32" height="32"/>
  </tile>
  <tile id="72">
-  <image source="../../Sprites/Swamp/73.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/073.png" width="32" height="32"/>
  </tile>
  <tile id="73">
-  <image source="../../Sprites/Swamp/74.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/074.png" width="32" height="32"/>
  </tile>
  <tile id="74">
-  <image source="../../Sprites/Swamp/75.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/075.png" width="32" height="32"/>
  </tile>
  <tile id="75">
-  <image source="../../Sprites/Swamp/76.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/076.png" width="32" height="32"/>
  </tile>
  <tile id="76">
-  <image source="../../Sprites/Swamp/77.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/077.png" width="32" height="32"/>
  </tile>
  <tile id="77">
-  <image source="../../Sprites/Swamp/78.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/078.png" width="32" height="32"/>
  </tile>
  <tile id="78">
-  <image source="../../Sprites/Swamp/79.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/079.png" width="32" height="32"/>
  </tile>
  <tile id="79">
-  <image source="../../Sprites/Swamp/80.png" width="32" height="32"/>
+  <image source="../../Sprites/Swamp/080.png" width="32" height="32"/>
  </tile>
  <tile id="80">
   <image source="../../Sprites/Swamp/081.png" width="32" height="32"/>
